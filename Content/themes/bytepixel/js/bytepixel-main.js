036b4d43420c54d66ef8cf473ee8f31160ec91a6
Guillermo Pelagio <guillermo.pelagio.hdz@gmail.com>
2025-11-02 09:54:15 -0600

    Subida a panel

--- Content/themes/bytepixel/js/bytepixel-main.js
+++ Content/themes/bytepixel/js/bytepixel-main.js
@@ -3,7 +3,7 @@ var urlActual = window.location + '';
 var arrayUrl = urlActual.split("/");
 
 //En desarrollo va el nombre de la carpeta antes del content
-//document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + ':44327/content/themes/bytepixel/js/bytepixel-funciones.js?version=' + aleatorio + '"></script>');
-document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + ':44327/content/themes/bytepixel/js/main.js?version=' + aleatorio + '"></script>');
+//document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + '/content/themes/bytepixel/js/bytepixel-funciones.js?version=' + aleatorio + '"></script>');
+document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + '/content/themes/bytepixel/js/main.js?version=' + aleatorio + '"></script>');
 //document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + '/bytepixel/content/themes/bytepixel/js/bytepixel-funciones.js?version=' + aleatorio + '"></script>');
 //document.write('<script src="' + window.location.protocol + "//" + window.location.hostname + '/bytepixel/content/themes/bytepixel/js/main.js?version=' + aleatorio + '"></script>');
